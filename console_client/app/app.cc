bbd363468edddf022e2bd69d347c52f69be65683
Dishan Philips <dishanphilips@gmail.com>
2020-06-25 14:25:20 +0400

    [CODE][APP] - Added async support.

--- console_client/app/app.cc
+++ console_client/app/app.cc
@@ -1,16 +1,18 @@
-#pragma once
-
 #include <iostream>
 #include <memory>
 #include <string>
 
 #include <grpcpp/grpcpp.h>
+#include <grpc/support/log.h>
+#include <thread>
 
-#include "../../core/gen/server.pb.h"
 #include "../../core/gen/server.grpc.pb.h"
+#include "../../core/gen/server.pb.h"
 
 using grpc::Channel;
+using grpc::ClientAsyncResponseReader;
 using grpc::ClientContext;
+using grpc::CompletionQueue;
 using grpc::Status;
 using JadeCore::CommandRequest;
 using JadeCore::CommandResponse;
@@ -18,18 +20,65 @@ using JadeCore::RpcBase;
 
 class GreeterClient {
 public:
-    GreeterClient(std::shared_ptr<Channel> channel) : stub_(RpcBase::NewStub(channel)) {}
+    explicit GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(RpcBase::NewStub(channel)) {}
 
-    std::string GetInfo(const std::string& message) {
-
-        JadeCore::InfoRequest* info = new JadeCore::InfoRequest();
-        info->set_message("jiji");
-    	
-    	// Data we are sending to the server.
-        CommandRequest request;
+    // Assembles the client's payload and sends it to the server.
+    void SayHello(const std::string& user) {
+        // Data we are sending to the server.
+		CommandRequest request;
         request.set_operation(1);
-        request.set_request(info->SerializeAsString());
+        request.set_request(user);
 
+        // Call object to store rpc data
+        AsyncClientCall* call = new AsyncClientCall;
+
+        // stub_->PrepareAsyncSayHello() creates an RPC object, returning
+        // an instance to store in "call" but does not actually start the RPC
+        // Because we are using the asynchronous API, we need to hold on to
+        // the "call" instance in order to get updates on the ongoing RPC.
+        call->response_reader = stub_->PrepareAsyncHandle(&call->context, &cq_);
+
+        // StartCall initiates the RPC call
+        call->response_reader->StartCall((void*)call);
+        call->response_reader->Write(request, (void*)call);
+
+    	// Request that, upon completion of the RPC, "reply" be updated with the
+        // server's response; "status" with the indication of whether the operation
+        // was successful. Tag the request with the memory address of the call object.
+        call->response_reader->Finish(&call->status, (void*)call);
+
+    }
+
+    // Loop while listening for completed responses.
+    // Prints out the response from the server.
+    void AsyncCompleteRpc() {
+        void* got_tag;
+        bool ok = false;
+
+        // Block until the next result is available in the completion queue "cq".
+        while (cq_.Next(&got_tag, &ok)) {
+            // The tag in this example is the memory location of the call object
+            AsyncClientCall* call = static_cast<AsyncClientCall*>(got_tag);
+
+            // Verify that the request was completed successfully. Note that "ok"
+            // corresponds solely to the request for updates introduced by Finish().
+            GPR_ASSERT(ok);
+
+            if (call->status.ok())
+                std::cout << "Greeter received: " << call->reply.response() << std::endl;
+            else
+                std::cout << "RPC failed" << std::endl;
+
+            // Once we're complete, deallocate the call object.
+            delete call;
+        }
+    }
+
+private:
+
+    // struct for keeping state and data information
+    struct AsyncClientCall {
         // Container for the data we expect from the server.
         CommandResponse reply;
 
@@ -37,55 +86,42 @@ public:
         // the server and/or tweak certain RPC behaviors.
         ClientContext context;
 
-        // The actual RPC.
-        auto handler = stub_->Handle(&context);
-        handler->Write(request);
-        handler->Read(&reply);
-        auto status = handler->Finish();
-    	
-        // Act upon its status.
-        if (status.ok()) {
-            return reply.response();
-        }
-        else {
-            std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-            return "RPC failed";
-        }
-    }
+        // Storage for the status of the RPC upon completion.
+        Status status;
 
-private:
+
+        std::unique_ptr<grpc::ClientAsyncReaderWriter<CommandRequest, CommandResponse>> response_reader;
+    };
+
+    // Out of the passed in Channel comes the stub, stored here, our view of the
+    // server's exposed services.
     std::unique_ptr<RpcBase::Stub> stub_;
+
+    // The producer-consumer queue we use to communicate asynchronously with the
+    // gRPC runtime.
+    CompletionQueue cq_;
 };
 
 int main(int argc, char** argv) {
-    std::string target_str;
-    std::string arg_str("--target");
-    if (argc > 1) {
-        std::string arg_val = argv[1];
-        size_t start_pos = arg_val.find(arg_str);
-        if (start_pos != std::string::npos) {
-            start_pos += arg_str.size();
-            if (arg_val[start_pos] == '=') {
-                target_str = arg_val.substr(start_pos + 1);
-            }
-            else {
-                std::cout << "The only correct argument syntax is --target=" << std::endl;
-                return 0;
-            }
-        }
-        else {
-            std::cout << "The only acceptable argument is --target=" << std::endl;
-            return 0;
-        }
+
+
+    // Instantiate the client. It requires a channel, out of which the actual RPCs
+    // are created. This channel models a connection to an endpoint (in this case,
+    // localhost at port 50051). We indicate that the channel isn't authenticated
+    // (use of InsecureChannelCredentials()).
+    GreeterClient greeter(grpc::CreateChannel(
+        "localhost:50051", grpc::InsecureChannelCredentials()));
+
+    // Spawn reader thread that loops indefinitely
+    std::thread thread_ = std::thread(&GreeterClient::AsyncCompleteRpc, &greeter);
+
+    for (int i = 0; i < 100; i++) {
+        std::string user("world " + std::to_string(i));
+        greeter.SayHello(user);  // The actual RPC call!
     }
-    else {
-        target_str = "127.0.0.1:50051";
-    }
-    GreeterClient greeter(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-    std::string message("Jade");
-    std::string reply = greeter.GetInfo(message);
-    std::cout << "Greeter received: " << reply << std::endl;
+
+    std::cout << "Press control-c to quit" << std::endl << std::endl;
+    thread_.join();  //blocks forever
 
     return 0;
 }
